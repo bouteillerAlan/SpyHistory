d3ec9480a07de6fb2ae0164545452487b36dd951
Alan Bouteiller <bouteiller.alan@gmail.com>
2019-08-20 19:30:56 +0200

    add link and desc

--- front/src/tuto_line.js
+++ front/src/tuto_line.js
@@ -2,19 +2,135 @@
 const tuto = {
     // season
     // My Story
-    '215AAA0F-CDAC-4F93-86DA-C155A99B5784' : '#a',
+    '215AAA0F-CDAC-4F93-86DA-C155A99B5784' : 'lien',
     // Scarlet's War
-    '002C2D90-69B5-41A2-A422-8DB6F2EFC53E' : '#a',
+    '002C2D90-69B5-41A2-A422-8DB6F2EFC53E' : 'lien',
     // Living World Season 2
-    'A515A1D3-4BD7-4594-AE30-2C5D05FF5960' : '#a',
+    'A515A1D3-4BD7-4594-AE30-2C5D05FF5960' : 'lien',
     // Heart of Thorns
-    'B8901E58-DC9D-4525-ADB2-79C93593291E' : '#a',
+    'B8901E58-DC9D-4525-ADB2-79C93593291E' : 'lien',
     // Living World Season 3
-    '09766A86-D88D-4DF2-9385-259E9A8CA583' : '#a',
+    '09766A86-D88D-4DF2-9385-259E9A8CA583' : 'lien',
     // Path of Fire
-    'EAB597C0-C484-4FD3-9430-31433BAC81B6' : '#a',
+    'EAB597C0-C484-4FD3-9430-31433BAC81B6' : 'lien',
     // Living World Season 4
-    'C22AFD21-667A-4AA8-8210-AC74EAEE58BB' : '#a'
+    'C22AFD21-667A-4AA8-8210-AC74EAEE58BB' : 'lien',
+
+    // story
+    // Mon histoire - Charr
+    '1' : 'lien',
+    // Mon histoire - Norn
+    '2' : 'lien',
+    // Mon histoire - Human
+    '3' : 'lien',
+    // Mon histoire - Sylvari
+    '7' : 'lien',
+    // Mon histoire - Asura
+    '8' : 'lien',
+    // Les Ordres de la Tyrie
+    '9' : 'lien',
+    // Le dragon ancestral Zhaïtan
+    '10' : 'lien',
+    // Leçon d'histoire de la saison 1
+    '39' : 'lien',
+    // 1. À l'orée de Maguuma
+    '11' : 'lien',
+    // 2. Emprise maléfique
+    '12' : 'lien',
+    // 3. L'ombre du dragon : 1re partie
+    '13' : 'lien',
+    // 4. L'ombre du dragon : 2e partie
+    '14' : 'lien',
+    // 5. Le spectre du temps
+    '15' : 'lien',
+    // 6. La voie des ronces
+    '16' : 'lien',
+    // 7. Les graines de la vérité
+    '17' : 'lien',
+    // 8. Le point de non-retour
+    '18' : 'lien',
+    // 1. Prologue : ralliement à Maguuma
+    '19' : 'lien',
+    // 2. Arrachés au ciel
+    '32' : 'lien',
+    // 3. Établir une base avancée
+    '41' : 'lien',
+    // 4. Dans la jungle
+    '34' : 'lien',
+    // 5. Sur leurs traces
+    '26' : 'lien',
+    // 6. Prisonniers du dragon
+    '33' : 'lien',
+    // 7. Précieux effets
+    '21' : 'lien',
+    // 8. Cité de l'espoir
+    '20' : 'lien',
+    // 9. Le chemin du prédateur
+    '35' : 'lien',
+    // 10. Curieuses observations
+    '31' : 'lien',
+    // 11. Racines de la terreur
+    '36' : 'lien',
+    // 12. En avant
+    '23' : 'lien',
+    // 13. Connaissances cachées
+    '22' : 'lien',
+    // 14. Découpe de signe
+    '42' : 'lien',
+    // 15. Récolte amère
+    '27' : 'lien',
+    // 16. Corps et âmes
+    '25' : 'lien',
+    // 1. Hors des ombres
+    '46' : 'lien',
+    // 2. L'Embrasement
+    '56' : 'lien',
+    // 3. Une fissure dans la glace
+    '63' : 'lien',
+    // 4. La Tête du serpent
+    '64' : 'lien',
+    // 5. Point d'ignition
+    '65' : 'lien',
+    // 6. À la fin du chemin
+    '66' : 'lien',
+    // 1. Allumer la flamme
+    '83' : 'lien',
+    // 2. Ouvrir la voie
+    '67' : 'lien',
+    // 3. Nuit des incendies
+    '82' : 'lien',
+    // 4. Le sacrifice
+    '72' : 'lien',
+    // 5. Souvenirs cristallins
+    '79' : 'lien',
+    // 6. Sol consacré
+    '69' : 'lien',
+    // 7. Affronter la vérité
+    '80' : 'lien',
+    // 8. La marche à suivre
+    '68' : 'lien',
+    // 9. En partance
+    '71' : 'lien',
+    // 10. L'ennemi de mon ennemi
+    '75' : 'lien',
+    // 11. Bête de guerre
+    '76' : 'lien',
+    // 12. Tuer un dieu
+    '81' : 'lien',
+    // 13. Petite victoire (Épilogue)
+    '78' : 'lien',
+    // 1. Aube
+    '85' : 'lien',
+    // 2. Un bug dans le système
+    '86' : 'lien',
+    // 3. Longue vie à la liche
+    '87' : 'lien',
+    // 4. Guidés par une étoile
+    '88' : 'lien',
+    // 5. Quitte ou double
+    '89' : 'lien',
+    // 6. Guerre éternelle
+    '90' : 'lien'
 }
 
 export default tuto
